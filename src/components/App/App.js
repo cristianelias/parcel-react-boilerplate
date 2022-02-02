e7fac6769cb0c9771ffb5c6722d7f26f6309c706
Cris Elias <cristian.elias@mercadolibre.com>
2022-02-02 01:03:11 -0300

    Oh, hi

---
+++ src/components/App/App.js
@@ -0,0 +1,24 @@
+const App = () => (
+  <div
+    style={{
+      width: "100vh",
+      height: "100vh",
+      display: "flex",
+      "align-items": "center",
+      "justify-content": "center",
+      background: "#000000",
+    }}
+  >
+    <div style={{ color: "white" }}>
+      <h1>sup br0</h1>
+      <h2>let&apos;s get to work</h2>
+    </div>
+    <img
+      src="https://c.tenor.com/sTFc7j1xRJ0AAAAC/doge-dancing-doge.gif"
+      alt=""
+      style={{ width: "500px" }}
+    />
+  </div>
+);
+
+export default App;
